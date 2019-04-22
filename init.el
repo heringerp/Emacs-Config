bbb1f805768273170d3487a4c81796a50b2f186d
Peter Heringer <peter.heringer@gmx.com>
2019-04-22 16:21:48 +0200

    Added auctex

--- init.el
+++ init.el
@@ -11,16 +11,23 @@
 
 ;; Set theme
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (doom-Iosvkem)))
  '(custom-safe-themes
    (quote
     ("7e78a1030293619094ea6ae80a7579a562068087080e01c2b8b503b27900165c" default)))
  '(package-selected-packages
    (quote
-    (evil elscreen-multi-term helm-spotify-plus which-key use-package helm doom-themes))))
-(custom-set-faces)
+    (auctex evil elscreen-multi-term helm-spotify-plus which-key use-package helm doom-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
 
 ;; Load configuration.org via org-babel
 (org-babel-load-file "~/.emacs.d/configuration.org")
-
-
